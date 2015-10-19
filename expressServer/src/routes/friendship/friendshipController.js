869749c2d2ec4dcbe7bc93d7fd7510cd849e0c4b
André Botelho Almeida <almeidaa@ae.com>
2015-10-19 17:52:46 -0200

    Cleaning up frienship controller

--- expressServer/src/routes/friendship/friendshipController.js
+++ expressServer/src/routes/friendship/friendshipController.js
@@ -3,201 +3,149 @@ var friendshipDAO = require('../../persistent/friendshipDAO');
 
 var FriendshipController = function () { 
 
-  this.listAllFriendships = function(request, response) {
-    
-    var fail = function(error, data) {
-      console.log('Friendship Controller Error: ' + error);
+  var success = function(response) {
+    return function(data) {
+      if (data) {
+        response.json(data);
+      } else {
+        response.sendStatus(200);
+      }
+    };
+  };
+
+  var fail = function(errorMessage, response) {
+    return function(error, data) {
+      console.log('Friendship Controller - ' + errorMessage + error);
       response.status(403).send({error: error, data: data});
     };
-    var success = function(data) {
-      response.status(200).json(data);
+  };
+
+  var failWithEmptyResponse = function(errorMsg, response) {
+    return function(error) {
+      console.log('Friendship Controller - ' + errorMsg + error);
+      console.log('Returning empty results');
+      response.status(200).json({});
     };
+  };
+
+  this.listAllFriendships = function(request, response) {
+    var errorMsg = 'List All Friendships - Error: ';
 
     console.log('Getting all friendships.');
-    friendshipDAO.listAllFriendships(success, fail);
-    
+
+    friendshipDAO.listAllFriendships(success(response), fail(errorMsg, response));
   };
   
   this.listMyFriendships = function(request, response) {
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - List My Friendships - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };
+    var errorMsg = 'List My Friendships - Error: ';
 
     console.log('Getting my friendships.');
-    friendshipDAO.listAllMyFriendships(token._id, success, fail);
-    
+
+    friendshipDAO.listAllMyFriendships(token._id, success(response), fail(errorMsg, response));
   };
 
   this.listFriendshipsRequested = function(request, response) {
     var token = request.token;
-    var fail = function(error) {
-      if(error) {
-        console.log('Friendship Controller Error: ' + error);
-        console.log('Returning empty results');
-        response.status(200).json({});
-      }
-    };
-    var success = function(data) {
-      if(data) {
-        console.log('Returning result: ' + data);
-        response.status(200).json(data);
-      }
-    };
+    var errorMsg = 'List Friendships Requested: ';
 
     console.log('Getting Friendships that logged user requested.');
-    friendshipDAO.getFriendshipRequested(token._id, success, fail);
+
+    friendshipDAO.getFriendshipRequested(token._id, success(response), failWithEmptyResponse(errorMsg, response));
   };
 
   this.listFriendshipsRequests = function(request, response) {
     var token = request.token;
-    var fail = function(error) {
-      if(error) {
-        console.log('Friendship Controller Error: ' + error);
-        console.log('Returning empty results');
-        response.status(200).json({});
-      }
-    };
-    var success = function(data) {
-      if(data) {
-        console.log('Returning result: ' + data);
-        response.status(200).json(data);
-      }
-    };
+    var errorMsg = 'List Friendships Requests: ';
 
     console.log('Getting Friendships that logged user received.');
-    friendshipDAO.getFriendshipRequests(token._id, success, fail);
+
+    friendshipDAO.getFriendshipRequests(token._id, success(response), failWithEmptyResponse(errorMsg, response));
   };
 
   this.getFriendship = function(request, response) {  	
-	var token = request.token;
+	  var token = request.token;
     var friendId = request.params.friendId;
-    var fail = function(error) {
-      if(error) {
-        console.log('Friendship Controller Error: ' + error);
-        console.log('Returning empty result');
-        response.status(200).json({});
-      }
-    };
-    var success = function(data) {
-      if(data) {
-        console.log('Returning result: ' + data);
-        response.status(200).json(data);
-      }
-    };
+    var errorMsg = 'Get Friendship: ';
+
     console.log('Getting Friendship with Friends_Id #' + friendId);
-    friendshipDAO.getFriendship(token._id, friendId, success, fail);
+
+    friendshipDAO.getFriendship(token._id, friendId, success(response), failWithEmptyResponse(errorMsg, response));
   };
   
   
   this.inviteFriend = function(request, response) {
 	  var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - Invite Friend - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Invite Friend :';
+
     console.log('Inviting one friend');
-    friendshipDAO.createFriendship(token._id, friendId, success, fail);
+
+    friendshipDAO.createFriendship(token._id, friendId, success(response), fail(errorMsg, response));
   };
 
   this.acceptFriend = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - acceptFriend - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Accept Friend: ';
+
     console.log('Accepting a friend');
-    friendshipDAO.acceptFriendship(token._id, friendId, success, fail);
+
+    friendshipDAO.acceptFriendship(token._id, friendId, success(response), fail(errorMsg, response));
   };
 
   this.rejectFriend = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - rejectFriend - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Reject Friend: ';
+
     console.log('Rejecting a friend');
-    friendshipDAO.rejectFriendship(token._id, friendId, success, fail);
+
+    friendshipDAO.rejectFriendship(token._id, friendId, success(response), fail(errorMsg, response));
   };
 
   this.setVip = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - setVip - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Set VIP: ';
+
     console.log('set friend as vip');
-    friendshipDAO.updateFriendshipVipStatus(token._id, friendId, true, success, fail);
+
+    friendshipDAO.updateFriendshipVipStatus(token._id, friendId, true, success(response), fail(errorMsg, response));
   };
 
 
   this.unsetVip = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - unsetVip - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Unset VIP: ';
+
     console.log('unset friend as vip');
-    friendshipDAO.updateFriendshipVipStatus(token._id, friendId, false, success, fail);
+
+    friendshipDAO.updateFriendshipVipStatus(token._id, friendId, false, success(response), fail(errorMsg, response));
   };
 
 
   this.setBlock = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - setBlock - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Set Block: ';
+
     console.log('block friend');
-    friendshipDAO.updateFriendshipBlockStatus(token._id, friendId, true, success, fail);
+
+    friendshipDAO.updateFriendshipBlockStatus(token._id, friendId, true, success(response), fail(errorMsg, response));
   };
 
 
   this.unsetBlock = function(request, response) {
     var friendId = request.params.friendId;
     var token = request.token;
-    var fail = function(error, data) {
-      console.log('Friendship Controller - unsetBlock - Error: ' + error);
-      response.status(403).send({error: error, data: data});
-    };
-    var success = function(data) {
-      response.status(200).json(data);
-    };  
+    var errorMsg = 'Unset Block: ';
+
     console.log('unblock friend');
-    friendshipDAO.updateFriendshipBlockStatus(token._id, friendId, false, success, fail);
+
+    friendshipDAO.updateFriendshipBlockStatus(token._id, friendId, false, success(response), fail(errorMsg, response));
   };
-
-
-
-
 };
 
 module.exports = new FriendshipController();
